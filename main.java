59b22c97b6c261e3737760a27fbd19db2e19d1ff
Katherine Nicholson <katherine.nicholson97@gmail.com>
2023-07-12 03:38:02 +0100

    added main class to java

--- main.java
+++ main.java
@@ -1,4 +1,6 @@
 public Class Main {
+    public static void main(String [] args) {
     System.out.println("Hello world");
-    System.out.println("Testing print statement");
+    System.out.println("Testing print statement")
+    }
 }
